f03b68cc0123905098eaf8e5dc23eef49d253df9
Fish Yu <yhl10000@gmail.com>
2014-07-15 21:59:49 +0800

    2014-07-15 21:59:46

--- scripts/travis/run_scripts.sh
+++ scripts/travis/run_scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 xctool -workspace Fish.xcworkspace -scheme Fish -sdk iphonesimulator   test
-xctool -workspace Fish.xcworkspace -scheme Fish -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR='~/build/' build
+xctool -workspace Fish.xcworkspace -scheme Fish -configuration Release -sdk iphoneos7.1 CONFIGURATION_BUILD_DIR='~/build/' build
